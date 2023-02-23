4b0650ffc8dbfa582d1fc34b53a16f50c7364fb6
Cristhel Quito <80082868+Cristhel17@users.noreply.github.com>
2023-02-22 20:42:23 -0500

    Se realizo cambio en la linea 96 de la clase ChessGameBoard.java

--- Original.src/ChessGameBoard.java
+++ Original.src/ChessGameBoard.java
@@ -93,7 +93,7 @@ public class ChessGameBoard extends JPanel{
      * @return ArrayList<GamePiece> the pieces
      */
     public ArrayList<ChessGamePiece> getAllBlackPieces(){
-        ArrayList<ChessGamePiece> blackPieces = new ArrayList<ChessGamePiece>();
+        ArrayList<ChessGamePiece> blackPieces = new ArrayList<>();
         for ( int i = 0; i < 8; i++ ){
             for ( int j = 0; j < 8; j++ ){
                 if ( chessCells[i][j].getPieceOnSquare() != null
